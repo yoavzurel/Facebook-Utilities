e19a034cd9aed360094de3dedf4545f0cbf119fe
Yoav <yoavzurel@gmail.com>
2011-08-24 18:51:47 +0300

    dbHandler Update

--- FacebookUtilitiesWebForms/DataBaseHandler.cs
+++ FacebookUtilitiesWebForms/DataBaseHandler.cs
@@ -21,7 +21,7 @@ namespace FacebookUtilitiesWebForms
     /// </summary>
     public enum eTableApplication_User_Columns
     {
-        ID,
+        User_ID,
         User_First_Name,
         User_Last_Name,
         User_Full_Name,
@@ -39,7 +39,7 @@ namespace FacebookUtilitiesWebForms
     /// </summary>
     public enum eTable_Friends_To_Greet_Columns
     {
-        ID,
+        Friend_ID,
         Friend_First_Name,
         Friend_Last_Name,
         Friend_Full_Name,
@@ -83,35 +83,98 @@ namespace FacebookUtilitiesWebForms
         public bool IsUserInDataBase(User i_User)
         {
             bool result = false;
-            m_DbConnection.Open();
-            m_DbCommand = m_DbConnection.CreateCommand();
-            m_DbCommand.CommandText = string.Format
+            openConnection();
+
+              SqlDataReader dbReader = queryDataBase(string.Format
                 ("SELECT * FROM {0} WHERE {1} = {2}",
                 eDataBaseTables.Application_Users.ToString(),
-                eTableApplication_User_Columns.ID.ToString(),
-                i_User.Id);
-            SqlDataReader dbReader = m_DbCommand.ExecuteReader();
+                eTableApplication_User_Columns.User_ID.ToString(),
+                i_User.Id));
+
             if (dbReader.HasRows)
             {
                 result = true;
             }
 
-            dbReader.Close();
-            m_DbConnection.Close();
+            closeReaderAndConnection(dbReader);
             return result;
         }
 
         /// <summary>
+        /// closes the database reader and data base connection
+        /// </summary>
+        /// <param name="i_DbReader"></param>
+        private void closeReaderAndConnection(SqlDataReader i_DbReader)
+        {
+            i_DbReader.Close();
+            m_DbConnection.Close();
+        }
+
+        /// <summary>
+        /// opens the connection to db
+        /// </summary>
+        private void openConnection()
+        {
+            m_DbConnection.Open();
+        }
+
+        /// <summary>
+        /// this method returns an sql database reader with the result of the given query
+        /// </summary>
+        /// <param name="i_Query"></param>
+        /// <returns></returns>
+        private SqlDataReader queryDataBase(string i_Query)
+        {
+            m_DbCommand = m_DbConnection.CreateCommand();
+            m_DbCommand.CommandText = i_Query;
+            SqlDataReader dbReader = m_DbCommand.ExecuteReader();
+            return dbReader;
+        }
+
+        /// <summary>
         /// This method returns the friends of the user that are already in the database.
-        /// Returns a dictionary by {ID,Friend}
+        /// Returns a dictionary by {ID,Friend}.
+        /// if the user dosn't have any friends in the DB, The count field of the dictionary will 
+        /// be 0
         /// </summary>
         /// <param name="i_User"></param>
         /// <returns></returns>
         public Dictionary<string, Friend> GetUserFriendsThatAreInDataBase(User i_User)
         {
             Dictionary<string, Friend> result = new Dictionary<string, Friend>();
+            i_User.Id = "2";
+            openConnection();
+            string query = string.Format(
+                "SELECT {0}, {1} FROM {2} WHERE {3} = {4} AND {5} = {6} AND {7} = {8}",
+                string.Join(", ", Enum.GetNames(typeof(eTable_Friends_To_Greet_Columns))),
+                eTbale_Birthday_Messages_Columns.Birthday_Greet.ToString(),
+                string.Join(", ", Enum.GetNames(typeof(eDataBaseTables))),
+                string.Join(".", new string[] { eDataBaseTables.Birthday_Messages.ToString(), eTbale_Birthday_Messages_Columns.Application_User_ID.ToString() }),
+                string.Join(".", new string[] { eDataBaseTables.Application_Users.ToString(), eTableApplication_User_Columns.User_ID.ToString() }),
+                string.Join(".", new string[] { eDataBaseTables.Birthday_Messages.ToString(), eTbale_Birthday_Messages_Columns.Friend_To_Greet_ID.ToString() }),
+                string.Join(".", new string[] { eDataBaseTables.Friends_To_Greet.ToString(), eTable_Friends_To_Greet_Columns.Friend_ID.ToString() }),
+                string.Join(".", new string[] { eDataBaseTables.Application_Users.ToString(), eTableApplication_User_Columns.User_ID.ToString() }),
+                i_User.Id);
+            SqlDataReader dbReader = queryDataBase(query);
+            while (dbReader.Read())
+            {
+                Friend tempFriend = new Friend();
+                tempFriend.Id = (string)dbReader[eTable_Friends_To_Greet_Columns.Friend_ID.ToString()];
+                tempFriend.FirstName = (string)dbReader[eTable_Friends_To_Greet_Columns.Friend_First_Name.ToString()];
+                tempFriend.LastName = (string)dbReader[eTable_Friends_To_Greet_Columns.Friend_Last_Name.ToString()];
+                tempFriend.FullName = (string)dbReader[eTable_Friends_To_Greet_Columns.Friend_Full_Name.ToString()];
+                tempFriend.Birthday = ((DateTime)dbReader[eTable_Friends_To_Greet_Columns.Friend_Birthday_Date.ToString()]).ToShortDateString();
+                tempFriend.BirthdayMessage = (string)dbReader[eTbale_Birthday_Messages_Columns.Birthday_Greet.ToString()];
+                
+                Dictionary<string, string> tempFriendPics = new Dictionary<string, string>();
+                tempFriendPics[ePictureTypes.pic.ToString()] = (string)dbReader[eTable_Friends_To_Greet_Columns.Friend_Pic.ToString()];
+                tempFriendPics[ePictureTypes.pic_big.ToString()] = (string)dbReader[eTable_Friends_To_Greet_Columns.Friend_Pic_Big.ToString()];
+                tempFriendPics[ePictureTypes.pic_small.ToString()] = (string)dbReader[eTable_Friends_To_Greet_Columns.Friend_Pic_Small.ToString()];
+                tempFriendPics[ePictureTypes.pic_square.ToString()] = (string)dbReader[eTable_Friends_To_Greet_Columns.Friend_Pic_Square.ToString()];
+                tempFriend.Pictures = tempFriendPics;
+                result[tempFriend.Id] = tempFriend;
+            } 
             return result;
         }
-
     }
 }
